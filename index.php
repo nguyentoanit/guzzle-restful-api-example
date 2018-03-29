e04fbb8b7536c79978859274a720a2694e895b3f
Fuji_Toan <nguyenvantoantha.hua@gmail.com>
2018-03-29 15:02:48 +0700

    POST, PUT, DELETE method

--- index.php
+++ index.php
@@ -8,6 +8,34 @@
 	    // You can set any number of default request options.
 	    'timeout'  => 2.0,
 	]);
-	$response = $client->request('GET', '/api/users/2');
+	//GET method
+	$response = $client->request('GET', '/api/users');
 	$body = $response->getBody()->getContents();
+	echo "$body\n\n";
+	
+	//POST method
+	$response = $client->request('POST', '/api/register', [
+		'form_params' => [
+			'email' => 'sydney@fife',
+			'password' => 'pistol'
+		]
+	]);
+	$body = $response->getBody()->getContents();
+	echo "$body\n\n";
+	
+	//PUT method
+	$response = $client->request('PUT', '/api/register', [
+		'form_params' => [
+			'name' => 'morpheus',
+			'job' => 'zion resident'
+		]
+	]);
+	$body = $response->getBody()->getContents();
+	echo "$body\n\n";
+	
+	//Response code = 204
+	//DELETE method
+	$response = $client->request('DELETE', '/api/users/2');
+	$body = $response->getBody()->getContents();
+	echo $response->getStatusCode()."\n\n";
 ?>
